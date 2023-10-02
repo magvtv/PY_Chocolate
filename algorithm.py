788fb14cc675beec2ee7713d94e6c5bd54848ce5
magvtv <magutujr@gmail.com>
2023-10-02 17:27:17 +0300

    got bored with implementing linked list

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,32 @@
 # wanna learn on algorithms with python. just one would do.
+"""
+    states in the usa: Washington, Illnois, Mississippi, Texas, Nebraska, Arizona, Wyoming
+    South Dakota, North Carolina, Utah, Iowa, Alburquque
+"""
+    
+class Node:
+    def __init__(self, element, next):
+        self.element = element
+        self.next = next
+def __init__(self):
+    self.head = None
+    self.size = 0
+def __len__(self):
+    return self.size
+def is_empty(self):
+    return self.size == 0
+
+def push(self, x):
+    self.head = self.Node(x, self.head)
+    self.size += 1
+    
+def top(self):
+    if self.is_empty():
+        raise "Linked list is empty"
+    return self.head.element
+    
+    
+node1 = Node("LAX", "MSP")
+node2 = Node("MSP", "SDT")
+node3 = Node("SDT", "TXS")
+
