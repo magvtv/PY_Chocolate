0b9c7c6ebce12661aad23212eb8621c41c97a2ca
magvtv <magutujr@gmail.com>
2023-10-17 07:48:18 +0300

    Revert "finished updating game, everything works seamlessly"
    This reverts commit 7652d7346988e0f8d0a1d52aa65d26560268022f.

--- RockPaperScissors/rock_paper_scissors.py
+++ RockPaperScissors/rock_paper_scissors.py
@@ -1,34 +1,23 @@
 import random
 
+
 def victory(player, opponent):
-    if (((player == 'Rock') and (opponent == 'Scissors')) or 
-        ((player == 'Scissors') and (opponent == 'Paper'))  or 
-        ((player == 'Paper') and (opponent == 'Rock'))):
+    if (((player == 'rock') and (opponent == 'scissors')) or ((player == 'scissors') and (opponent == 'paper')) or ((player == 'paper') and (opponent == 'rock'))):
         return True
 
-def playGame():
-    user_choice = input("1.Rock 2.Paper 3.Scissors \nEnter choice number: ")    
-    game_options = {
-        '1': 'Rock',
-        '2': 'Paper',
-        '3': 'Scissors',
-    }
 
-    if user_choice in game_options:
-        user_choice = game_options[user_choice]
-    else:
-        while user_choice not in game_options:
-            print("Invalid choice! Please enter 1, 2 or 3.")
-            user_choice =  input("1.Rock 2.Paper 3.Scissors \nEnter choice number: ")
-    
-    CPU_choice = random.choice(list(game_options.values()))
-    
-    if user_choice == CPU_choice:
-        return f"A draw! I chose {CPU_choice} too!"
-    elif victory(user_choice, CPU_choice):
-        return f"You win! I chose {CPU_choice}"
-    else:
-        return f"You lost! I chose {CPU_choice}"
+def playGame():
+    user = input("Enter rock, paper, scissors: ").lower()
+    CPU = random.choice(['rock', 'paper', 'scissors'])
+
+    if CPU == user:
+        return "Draw!"
+
+    # R beats > S, S beats > P, P beats > R
+    if victory(user, CPU):
+        return "You Win!"
+
+    return "You Lose! "
 
 
 print(playGame())
