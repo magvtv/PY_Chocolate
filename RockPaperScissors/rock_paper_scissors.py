cce5989ab575324f0b986c64c3880a97c9ddbf3a
magvtv <magutujr@gmail.com>
2023-10-17 08:07:20 +0300

    better rps game

--- RockPaperScissors/rock_paper_scissors.py
+++ RockPaperScissors/rock_paper_scissors.py
@@ -1,23 +1,32 @@
 import random
 
-
 def victory(player, opponent):
-    if (((player == 'rock') and (opponent == 'scissors')) or ((player == 'scissors') and (opponent == 'paper')) or ((player == 'paper') and (opponent == 'rock'))):
+    if ((player == 'Rock' and opponent == 'Scissors') or
+        (player == 'Scissors' and opponent == 'Paper') or
+        (player == 'Paper' and opponent == 'Rock')):
         return True
 
-
 def playGame():
-    user = input("Enter rock, paper, scissors: ").lower()
-    CPU = random.choice(['rock', 'paper', 'scissors'])
+    user_choice = input("1.Rock 2.Paper 3.Scissors \nEnter choice number: ")
+    game_options = {
+        '1': 'Rock',
+        '2': 'Paper',
+        '3': 'Scissors'
+    }
+    
+    if user_choice in game_options:
+        user_choice = game_options[user_choice]
+    else:
+        print("Invalid choice. Please enter 1, 2, or 3!")
+        user_choice = input("1.Rock 2.Paper 3.Scissors \nEnter choice number: ")
 
-    if CPU == user:
-        return "Draw!"
+    CPU_choice = random.choice(list(game_options.values()))
 
-    # R beats > S, S beats > P, P beats > R
-    if victory(user, CPU):
-        return "You Win!"
-
-    return "You Lose! "
-
+    if CPU_choice == user_choice:
+        return f"A draw! \nI chose {CPU_choice.lower()} too!"
+    elif victory(user_choice, CPU_choice):
+        return f"You Won! \nI chose {CPU_choice.lower()}."
+    else:
+        return f"You Lost! \nI chose {CPU_choice.lower()}."
 
 print(playGame())
