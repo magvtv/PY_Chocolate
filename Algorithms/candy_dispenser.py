8714eddc23eabb6a26ca498b623d156f5f81a5af
magvtv <magutujr@gmail.com>
2023-10-19 05:18:43 +0300

    finished the app, cant run

--- Algorithms/candy_dispenser.py
+++ Algorithms/candy_dispenser.py
@@ -19,16 +19,64 @@ class PezDispenser:
         else:
             return None
     
-    #revert the suggested instruction on stack 
+    #revert the suggested action on stack 
     def undo(self):
         if self.undo_stack:
             operation, candy = self.undo_stack.pop()
             if operation == 'push':
-                # undo the push instruction by removing the candy
+                # undo the push action by removing the candy
                 self.stack.pop()
             elif operation == 'pop':
-                # undo the pop instruction by returning the candy
+                # undo the pop action by returning the candy
                 self.stack.append(candy)
                 
     def is_empty(self):
         return len(self.stack) == 0
+    
+class CandyDispenserApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title = ('PEZ Candy Dispenser')
+        self.pez_dispenser = PezDispenser()
+        self.label = tk.Label(root, text='PEZ Candy Dispenser')
+        self.label.pack
+        
+        self.candy_entry = tk.Entry(root)
+        self.candy_entry.pack()
+        
+        self.push.button = tk.Button(root, text='PUSH', command=self.push_candy)
+        self.push_button.pack()
+        
+        self.pop.button = tk.Button(root, text='POP', command=self.pop_candy)
+        self.pop_button.pack()
+        
+        self.undo.button = tk.Button(root, text='UNDO', command=self.undo_action)
+        self.undo_button.pack()
+    
+    def push_candy(self):
+        candy = self.candy_entry.get()
+        if candy:
+            self.pez_dispenser.push(candy)
+            self.candy_entry.delete(0, tk.END)
+
+    def pop_candy(self):
+        candy = self.pez_dispenser.pop()
+        if candy:
+            message = f'Popped: {candy}'
+        else:
+            message = f'Dispenser is empty'
+
+    def undo_action(self):
+        self.pez_dispenser.undo()
+        self.show_message("Undo")
+        
+    def show_message(self, message):
+        message_label = tk.Label(self.root, text=message)
+        message_label.pack()
+        
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = CandyDispenserApp(root)
+    root.mainloop()
+    
+    
