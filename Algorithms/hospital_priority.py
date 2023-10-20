4e0e4516b842e3412f8b102225464e41feec6175
magvtv <magutujr@gmail.com>
2023-10-20 08:39:24 +0300

    no need for duration, simply time to check in

--- Algorithms/hospital_priority.py
+++ Algorithms/hospital_priority.py
@@ -2,6 +2,9 @@
     implementing a hospital priority queue using the priority queue ADT
     thoughts on mankone together with scheduling people according to their
     appointment times
+    add name and appointment time
+    sort with reference to the early bird, the earliest appointment
+    
 """
 
 import tkinter as tk
@@ -11,32 +14,18 @@ class HospitalQueue:
     def __init__(self):
         self.queue = []
         
-    def add_patient(self, name, start_time, end_time):
+    def add_patient(self, name, appointment_time):
         # calculate the duration of the appointment
         fmt = '%H%Mh'
-        start_datetime = datetime.strptime(start_time, fmt)
-        end_datetime = datetime.strptime(end_time, fmt)
-        
-        # duration in minutes first then conditional to control bigger duration into hours and minutes
-        duration_mins = ((end_datetime - start_datetime).total_seconds()) / 60
-        
-        if duration_mins >= 60:
-            duration_hr = duration_mins // 60
-            duration_rem_mins = duration_mins % 60
-            duration_str = f'{int(duration_hr)}h {int(duration_rem_mins)}m'
-        else:
-            duration_str = f'{int(duration_mins)}m'
-            
-        
-        
-        
-        self.queue.append((name, start_time, end_time, duration_str))
+        start_datetime = datetime.strptime(appointment_time, fmt)
+        self.queue.append((name, appointment_time))
         self.queue.sort()
         
     def remove_next_patient(self):
         if not self.is_empty():
             return self.queue.pop(0)
         return None
+    
     def is_empty(self):
         return len(self.queue) == 0
     
@@ -44,20 +33,19 @@ class HospitalQueue:
         return len(self.queue)
     
 root = tk.Tk()
-root.title = 'Checkup Priority Queue'
+root.title = ('Appointment Checkup')
 
 queue = HospitalQueue()
 
 def refresh_listbox():
     patient_listbox.delete(0, tk.END)
-    for name, start_time, end_time, duration_str in queue.queue:
-        patient_listbox.insert(tk.END, f'{name.capitalize()}: {duration_str} - [{start_time} - {end_time}]')
+    for name, appointment_time in queue.queue:
+        patient_listbox.insert(tk.END, f'{name.capitalize()}: {appointment_time}')
 
 def add_patient():
     name = name_entry.get()
-    start_time = start_time_entry.get()
-    end_time = end_time_entry.get()
-    queue.add_patient(name, start_time, end_time)
+    appointment_time = appointment_time_entry.get()
+    queue.add_patient(name, appointment_time)
     refresh_listbox()
     
 
@@ -72,15 +60,10 @@ name_label.pack()
 name_entry = tk.Entry(root)
 name_entry.pack()
 
-start_time_label = tk.Label(root, text='Start Time (HHmm):')
-start_time_label.pack()
-start_time_entry = tk.Entry(root)
-start_time_entry.pack()
-
-end_time_label = tk.Label(root, text='End Time (HHmm):')
-end_time_label.pack()
-end_time_entry = tk.Entry(root)
-end_time_entry.pack()
+appointment_time_label = tk.Label(root, text='Booking Time (HHmm):')
+appointment_time_label.pack()
+appointment_time_entry = tk.Entry(root)
+appointment_time_entry.pack()
 
 add_button = tk.Button(root, text='Add', command=add_patient)
 add_button.pack()
