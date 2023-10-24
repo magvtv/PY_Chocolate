cadfc9453eb3e2af02f9d9bfe50bc94051544025
magvtv <magutujr@gmail.com>
2023-10-24 18:41:58 +0300

    have a demo py file within KMC to testing of defaults

---
+++ KMC/seat_demo.py
@@ -0,0 +1,181 @@
+# Define cinema locations and showtimes
+cinema_locations = {
+    "Cinema1": "Nairobi",
+    "Cinema2": "Mombasa",
+    "Cinema3": "Kisumu",
+    "Cinema4": "Nakuru",
+    "Cinema5": "Eldoret",
+    "Cinema6": "Thika",
+}
+
+showtimes = {
+    "0900": "Morning Show",
+    "1400": "Afternoon Show",
+    "1900": "Evening Show",
+}
+
+# Define seat availability for each cinema location
+cinema_seats = {
+    "Cinema1": {
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "X"],
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "C": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "D": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "E": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "F": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "G": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "H": ["X", "X", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "I": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "J": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "K": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "L": ["O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"]
+    },
+    "Cinema2": {
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "X"],
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "C": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "D": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "E": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "F": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "G": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "H": ["X", "X", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "I": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "J": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "K": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "L": ["O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"]
+    },
+    "Cinema3": {
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "X"],
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "C": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "D": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "E": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "F": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "G": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "H": ["X", "X", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "I": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "J": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "K": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "L": ["O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"]
+    },
+    "Cinema4": {
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "X"],
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "C": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "D": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "E": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "F": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "G": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "H": ["X", "X", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "I": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "J": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "K": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "L": ["O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"]
+    },
+    "Cinema5": {
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "X"],
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "C": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "D": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "E": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "F": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "G": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "H": ["X", "X", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "I": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "J": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "K": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "L": ["O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"]
+    },
+    "Cinema6": {
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "X"],
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "C": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "D": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "E": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "F": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "G": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "H": ["X", "X", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "I": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "J": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "K": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "L": ["O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"]
+    },
+    # Define seat availability for other cinema locations
+}
+
+# Define ticket prices by category
+ticket_prices = {
+    "adult": 800,
+    "senior": 550,
+    "child": 350,
+}
+
+def display_seating(cinema, showtime):
+    seats = cinema_seats[cinema]
+    print(f"Available Seats for {showtimes[showtime]} at {cinema}:")
+    for row, seats_available in seats.items():
+        print(f"Row {row}: {' '.join(seats_available)}")
+
+# Main reservation function
+def reserve_seat():
+    # Get user's location choice
+    print("Select a cinema location:")
+    for cinema, town in cinema_locations.items():
+        print(f"{cinema} - {town}")
+    
+    cinema_choice = input("Enter the cinema number (e.g., Cinema1): ")
+    
+    if cinema_choice not in cinema_locations:
+        print("Invalid cinema location. Please try again.")
+        return
+    
+    # Get user's showtime choice
+    print("Select a showtime:")
+    for showtime, showtime_name in showtimes.items():
+        print(f"{showtime} - {showtime_name}")
+    
+    showtime_choice = input("Enter the showtime (e.g., 0900): ")
+    
+    if showtime_choice not in showtimes:
+        print("Invalid showtime. Please try again.")
+        return
+    
+    # Display available seats
+    display_seating(cinema_choice, showtime_choice)
+
+    # Get seat selection
+    row = input("Enter the row (e.g., A, B, L): ")
+    seat_number = int(input(f"Enter the seat number (1-{len(cinema_seats[cinema_choice][row])}): ")) - 1
+
+    if seat_number < 0 or seat_number >= len(cinema_seats[cinema_choice][row]):
+        print("Invalid seat number. Please try again.")
+        return
+
+    seat = cinema_seats[cinema_choice][row][seat_number]
+
+    # Get ticket category
+    print("Select a ticket category (adult/senior/child):")
+    category = input("Enter the category: ").lower()
+    
+    if category not in ticket_prices:
+        print("Invalid category. Please try again.")
+        return
+
+    # Calculate and display the total price
+    price = ticket_prices[category]
+    print(f"Ticket for {showtimes[showtime_choice]} at {cinema_choice} - Row {row}, Seat {seat_number + 1} ({category.capitalize()}): KES{price:.2f}")
+
+# Start reservation
+reserve_seat()
--- KMC/seat_reservation.py
+++ KMC/seat_reservation.py
@@ -1,4 +1,4 @@
-theatre_location = {
+theater_location = {
     '001': 'mombasa',
     '002': 'nanyuki',
     '003': 'nairobi',
@@ -6,3 +6,44 @@ theatre_location = {
     '005': 'kisumu',
     '006': 'thika',
 }
+
+showtimes = {
+    "0900h-1100h": "Early Bird",
+    "1115h-1315h": "Regular Viewer",
+    "1330h-1530h": "Matinee Goers",
+    "1545h-1745h": "Twilight Watchers",
+    "1800h-2000h": "Red Carpeters",
+    "2015h-2215h": "Night Owls"
+}
+
+ticket_prices = {
+    "Adults": 800,
+    "Seniors": 550,
+    "Child": 350,
+}
+
+theater_seats = {
+    "Cinema1": {
+        "A": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"],
+        "B": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "C": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "D": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "E": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "F": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "G": ["X", "X", "O", "X", "X", "X", "O", "O", "O", "O", "X", "O", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "H": ["X", "X", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "I": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "J": ["X", "O", "O", "X", "X", "X", "O", "X", "O", "O", "X", "X", "O", "O", "O", "X", "X", "O", "O", "O", "O", "X"],
+        "K": ["X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X"],
+        "L": ["O", "O", "O", "X", "X", "X", "O", "O", "O", "O", "X", "X", "O", "O", "X"]
+    }
+}
+
+
+def display_seats(cinema, showtime):
+    seats = theater_seats[cinema]
+    print(f"Available seats for {showtimes[showtime]} at {cinema}")
+    for row, seats_available in seats.items():
+        print(f"Row: {row}: {''.join(seats_available)}")
+# def reserve_seat():
+# print(theater_seats)
