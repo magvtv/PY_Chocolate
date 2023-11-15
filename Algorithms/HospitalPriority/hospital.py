484cba5168ec80490ca3db9a27882992d71ef4ca
magvtv <magutujr@gmail.com>
2023-11-15 12:59:36 +0300

    added Handlee font, will soon add Jakarta

--- Algorithms/HospitalPriority/hospital.py
+++ Algorithms/HospitalPriority/hospital.py
@@ -1,10 +1,11 @@
 import tkinter as tk
-from tkinter import messagebox, Listbox, END, Button, Text
+from tkinter import ttk, messagebox, Listbox, END, Button, Text
 from turtle import width
 from priority_queue import PriorityQueue, validate_time_format, convert_to_24hr
 import random
 
-# import re
+
+font_path = "/home/pharoh/.fonts/h/Handlee/Handlee-Regular.ttf"
 
 
 def generate_patient_code():
@@ -25,7 +26,7 @@ def format_name(name):
 def add_appointment():
     patient_name = format_name(name_entry.get())
     if not patient_name:
-        messagebox.showerror("Input Error", "Please enter patient name" )
+        messagebox.showerror("Input Error", "Please enter patient name")
     patient_code = (
         code_entry.get().upper() if code_entry.get() else generate_patient_code()
     )
@@ -53,7 +54,8 @@ def remove_next_patient():
     next_patient = queue.dequeue()
     if next_patient:
         messagebox.showinfo(
-            "Hospital Alert", f"Currently serving {next_patient['Name']}"
+            "Hospital Alert",
+            f"Currently serving... \nName: {next_patient['Name']} \nID: {next_patient['Code']}",
         )
         refresh_listbox()
     if queue.is_empty():
@@ -64,7 +66,7 @@ def refresh_listbox():
     # patients_listbox.delete(0, tk.END)
     patient_text.delete("1.0", END)
     for patient in queue:
-        formatted_text = f"Name: {patient['Name']} \nID: {patient['Code']} \nTime: {patient['Time']}h"
+        formatted_text = f"Name: {patient['Name']} \nID: {patient['Code']} \nTime: {patient['Time']}h\n\n"
         # patients_listbox.insert(END, formatted_text)
         patient_text.insert(END, formatted_text, "\n")
 
@@ -73,6 +75,10 @@ if __name__ == "__main__":
     queue = PriorityQueue()
     root = tk.Tk()
     root.title("Hospital Checkup Appointment")
+    root.option_add('*Font', f'{{Handlee}} {12}')
+    
+    font_style = ttk.Style()
+    font_style.configure("CustomFont.TFont", font=("Handlee", 'bold', 12))
 
     name_label = tk.Label(root, text="Patient Name:")
     name_label.pack()
@@ -80,7 +86,7 @@ if __name__ == "__main__":
     name_entry.pack()
 
     code_label = tk.Label(
-        root, text=f"Optional Patient Code (eg.{generate_patient_code()}):"
+        root, text=f"Optional Patient Code (eg. {generate_patient_code()}):"
     )
     code_label.pack()
     code_entry = tk.Entry(root)
@@ -97,7 +103,9 @@ if __name__ == "__main__":
     remove_button = Button(root, text="Serve Next Patient", command=remove_next_patient)
     remove_button.pack()
 
-    patient_text = Text(root, width=50, height=10)
+    patient_text = Text(root, width=50, height=10, wrap="word")
+    patient_text.tag_configure("CustomFont", font=("Handlee", 12))
+    patient_text.configure(font="CustomFont.TFont")
     patient_text.pack()
 
     refresh_listbox()
