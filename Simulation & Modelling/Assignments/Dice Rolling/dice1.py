0f63ff5f0b1f41e237c3667cb6343d4e594c0edf
magvtv <magutujr@gmail.com>
2024-02-27 12:40:56 +0300

    made two files, corrected the use of "int" previous one

--- Simulation & Modelling/Assignments/Dice Rolling/dice1.py
+++ Simulation & Modelling/Assignments/Dice Rolling/dice1.py
@@ -1,5 +1,6 @@
 import random
 import os
+import math
 
 # function to clear the terminal screen on linux os
 def clear_screen():
@@ -13,7 +14,7 @@ total_dice_rolls = 1000
 # initialize counters for each face of the dice
 face_counts = [0] * 6
 
-# simulating the rolling of the dice
+# simulating the rolling of the dice with for loop
 """
     for loop for updating the face counts, that have 6 elements, each
     set to 0.
@@ -25,26 +26,27 @@ for _ in range(total_dice_rolls):
 
     # Determine the face based on the random number
     """
-        The random number is used to determine which face of the dice was rolled. 
-        This is done by multiplying the random number by 6 (the number of faces on a dice)
-        adding 1 to ensure the result is between 1 and 6
+        the random number determines dice face to roll. 
+        multiplying the random number by 6 (the number of faces on a dice)
+        and adding 1 ensures the result is between 1 and 6 as it scales it to the possible 
+        range of faces on a six-faced die (a valid face)
     """
-    face = int(random_number * 6) + 1
+    # face = int(random_number * 6) + 1
+    face = random_number * 6 + 1
 
     # Update the counter for the corresponding face
     """
-        The counter for the corresponding face rolled is then updated by incrementing 
+        the counter for the corresponding face rolled is then updated by incrementing 
         the value at the index corresponding to that face in the face_counts list
     """
-    face_counts[face - 1] += 1
+    face_index = (math.floor(face) - 1)
+    face_counts[face_index] += 1
 
-# Display the table of outcomes such that
+# Display the table of outcomes
 """
     After all rolls are simulated, the face_counts list will contain 
     the frequency of each face rolled during the simulation
 """
-
-
 clear_screen()
 print("Outcome \tFrequency \tPercentage")
 print("-------------------------------------------")
@@ -52,4 +54,4 @@ for face, count in enumerate(face_counts, 1):
     percentage = (count / total_dice_rolls) * 100
     print(f"Face {face}\t\t{count}\t\t{percentage:.2f}%")
 print("-------------------------------------------")
-print(f"Total:\t\t{total_dice_rolls}\t\t{int(total_dice_rolls * 0.1)}%")
+print(f"Total:\t\t{total_dice_rolls}\t\t{100}%")
