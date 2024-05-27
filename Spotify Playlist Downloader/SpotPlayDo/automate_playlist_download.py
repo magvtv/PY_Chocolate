aae6e97c1822aa066f14f366333adccf59faf11f
magvtv <magutujr@gmail.com>
2024-05-28 00:21:04 +0300

    tried to manage browser state avoiding reopening and closing

--- Spotify Playlist Downloader/SpotPlayDo/automate_playlist_download.py
+++ Spotify Playlist Downloader/SpotPlayDo/automate_playlist_download.py
@@ -3,13 +3,12 @@ import json
 from playwright.sync_api import sync_playwright
 
 
-def download_song(song_link):
-    with sync_playwright() as p:
-        # launch brave to accomplish sequential actions
-        browser = p.chromium.launch(executable_path="/snap/bin/brave", headless=False)
-        page = browser.new_page()
+def download_song(page, song_link):
+    try:
+        print("Navigating to spotidown website")
         page.goto("https://spotifydown.com/")
         
+        print("Filling in the search box")
         search_box = page.locator("input[name='query']")
         search_box.fill(song_link)
         search_box.press("Enter")
@@ -22,14 +21,31 @@ def download_song(song_link):
         download_button.click()
         
         # wait 15s for download to complete
-        time.sleep(15000)
-        browser.close()
+        print("Awaiting song mp3 download to start")
+        page.wait_for_timeout(5000)
+    except Exception as e:
+        print(f"Error has occurred:\n{e}")
         
 if __name__ == '__main__':
     with open('songs.json', 'r') as file:
         songs = json.load(file)
-    for song in songs:
-        download_song(song['spotify_song_link'])
+        
+    with sync_playwright() as p:
+     # launch brave once to accomplish sequential actions in download_song function
+     browser = p.chromium.launch(executable_path="/snap/bin/brave", headless=False)
+     page = browser.new_page()
+     
+     song_count = len(songs)
+     index = 0
+     
+     while index < song_count:
+        song = songs[index]
+        print(f"Downloading:\n{song['title']} by {song['artist']}")
+        download_song(page, song['spotify_song_link'])
+        index += 1
+    
+    browser.close()
+    print("Browser has been closed!")
         
         
         
